39101c805de4921987a37e9785779b327a823a17
Dilshad <a-rustacean@outlook.com>
2024-01-25 22:49:39 +0530

    chore: add git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,3 +12,7 @@ sudo ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 sudo ln -s $HOME/.dotfiles/.zshenv $HOME/.zshenv
 sudo ln -s $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
 sudo ln -s $HOME/.dotfiles/.oh-my-zsh $HOME/.oh-my-zsh
+
+# git
+
+sudo ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig

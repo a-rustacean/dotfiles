af9da8e744bd95ebab5a31c09fcb81dcf23cb2e8
Dilshad <a-rustacean@outlook.com>
2024-01-25 22:44:15 +0530

    chore: add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# .config/
+
+sudo ln -s $HOME/.dotfiles/.config/cava $HOME/.config/cava
+sudo ln -s $HOME/.dotfiles/.config/helix $HOME/.config/helix
+sudo ln -s $HOME/.dotfiles/.config/neofetch $HOME/.config/neofetch
+
+# zsh & omz
+
+sudo ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+sudo ln -s $HOME/.dotfiles/.zshenv $HOME/.zshenv
+sudo ln -s $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
+sudo ln -s $HOME/.dotfiles/.oh-my-zsh $HOME/.oh-my-zsh

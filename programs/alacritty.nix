33ff4dea503ea39106d8ccaba96b9b4e39fc58fe
Dilshad <a-rustacean@outlook.com>
2025-10-14 19:58:49 +0530

    chore(alacritty): increase font size

--- programs/alacritty.nix
+++ programs/alacritty.nix
@@ -5,7 +5,7 @@
   settings = {
     general.live_config_reload = true;
     font = {
-      size = 18.0;
+      size = 24.0;
       bold = {
         family = "JetBrainsMono Nerd Font Mono";
         style = "Bold";

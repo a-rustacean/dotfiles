e933e875a9d6f8c2c8f09a7fab9977a40e01a940
Dilshad <a-rustacean@outlook.com>
2026-02-20 20:52:45 +0530

    chore(alacritty): use default colors

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -41,35 +41,5 @@ in
       };
       opacity = 0.85;
     };
-    colors = {
-      primary = {
-        background = "0x0d1117";
-        foreground = "0xc9d1d9";
-      };
-      cursor = {
-        text = "0x0d1117";
-        cursor = "0xc9d1d9";
-      };
-      normal = {
-        black = "0x484f58";
-        red = "0xff7b72";
-        green = "0x58a6ff";
-        yellow = "0xd29922";
-        blue = "0x58a6ff";
-        magenta = "0xbc8cff";
-        cyan = "0x39c5cf";
-        white = "0xb1bac4";
-      };
-      bright = {
-        black = "0x6e7681";
-        red = "0xffa198";
-        green = "0x79c0ff";
-        yellow = "0xe3b341";
-        blue = "0x79c0ff";
-        magenta = "0xbc8cff";
-        cyan = "0x39c5cf";
-        white = "0xb1bac4";
-      };
-    };
   };
 }

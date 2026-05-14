4b783cce8b6ac168d086f4925b9eb1e22baa75b3
Dilshad <a-rustacean@outlook.com>
2026-05-14 13:52:18 +0530

    feat(hosts/darwin): add setWallapaper activation

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -1,9 +1,17 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
   imports = [ ../../modules/home.nix ];
+
+  home.activation.setWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    /usr/bin/osascript -e "
+      tell application \"Finder\"
+        set desktop picture to POSIX file \"${../../wallpaper.jpg}\"
+      end tell"
+  '';
 
   services.gpg-agent = {
     enable = true;

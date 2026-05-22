9c26455293dda27d032e95307e2ce01e9ba297d5
Dilshad <a-rustacean@outlook.com>
2026-05-22 10:58:51 +0530

    darwin/home: add betterdisplay pkg

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -6,12 +6,16 @@
 {
   imports = [ ../../modules/home.nix ];
 
-  home.activation.setWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    /usr/bin/osascript -e "
-      tell application \"Finder\"
-        set desktop picture to POSIX file \"${../../wallpaper.jpg}\"
-      end tell"
-  '';
+  home = {
+    packages = [ pkgs.betterdisplay ];
+
+    activation.setWallpaper = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      /usr/bin/osascript -e "
+        tell application \"Finder\"
+          set desktop picture to POSIX file \"${../../wallpaper.jpg}\"
+        end tell"
+    '';
+  };
 
   services.gpg-agent = {
     enable = true;

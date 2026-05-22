b5f545b9ad6622bd76bf88a3b5bcf578d2996e26
Dilshad <a-rustacean@outlook.com>
2026-05-22 10:59:33 +0530

    modules/zen: add more a11y mods

--- modules/zen.nix
+++ modules/zen.nix
@@ -114,6 +114,8 @@ in
     };
     mods = [
       "4ab93b88-151c-451b-a1b7-a1e0e28fa7f8" # No Sidebar Scrollbar
+      "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+      "253a3a74-0cc4-47b7-8b82-996a64f030d5" # Floating History
     ];
     search = {
       force = true;

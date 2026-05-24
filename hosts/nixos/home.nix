35cb9ba996c538f4432b2debc82da20a803745f3
Dilshad <a-rustacean@outlook.com>
2026-05-24 21:19:17 +0530

    nixos/home: set gnome theme

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -39,4 +39,12 @@
   };
 
   services.hyprpolkitagent.enable = true;
+
+  dconf.settings."org/gnome/desktop/interface" = {
+    gtk-theme = "Adwaita";
+    icon-theme = "Flat-Remix-Red-Dark";
+    font-name = "Noto Sans Medium 11";
+    document-font-name = "Noto Sans Medium 11";
+    monospace-font-name = "Noto Sans Mono Medium 11";
+  };
 }

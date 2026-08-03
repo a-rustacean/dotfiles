e226a9f23a94f042fe455a1fb2fed7dc90f017de
Dilshad <a-rustacean@outlook.com>
2026-08-03 12:06:24 +0530

    nixos/hyprland: remove config

--- hyprland/env.lua
+++
@@ -1,8 +0,0 @@
--------------------------------
----- ENVIRONMENT VARIABLES ----
--------------------------------
-
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
